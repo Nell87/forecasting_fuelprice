f98e5f4de18753a07fb35958671488f51d9188b5
Sara <sara.marlop87@gmail.com>
2023-04-17 11:37:43 +0200

    test examples

---
+++ src/test_examples.py
@@ -0,0 +1,9 @@
+import pytest
+from collect_data import fuel_scraper_daily
+ 
+def test_data_numbercolumns():
+    """
+    Check that the number of columns is 2
+    """
+    data = fuel_scraper_daily(2022,2)
+    assert len(data.columns) == 2
